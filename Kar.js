4704123ea30c878c90c9f366044e80a4c8b6a235
Karliz24 <155625441+Karliz24@users.noreply.github.com>
2024-07-07 15:24:20 -0600

    Add files via upload

---
+++ Kar.js
@@ -0,0 +1,13 @@
+﻿"Nombre del catálogo: Método Pago, Fecha de creación :  jueves, 14  de  julio  de  2021 - 03:54p.m.     -      Número de registros : 11",,
+CD_MET_PAGO,NB_MET_PAGO
+1,Efectivo
+2,Transferencia
+3,Cheque
+4,Vales
+5,Tarjeta de débito
+6,Tarjeta de servicio
+7,No aplica
+8,Tarjeta bancaria canal cerrado
+9,Orden de Pago
+10,Cedula
+11,Ventanilla
